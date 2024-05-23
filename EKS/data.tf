ed42c4c7bfed1b66914f90ba4f2ae018751f6255
Mayank sharma <mayank14082003@gmail.com>
2024-05-24 02:56:30 +0530

    Update data.tf

--- EKS/data.tf
+++ EKS/data.tf
@@ -1,3 +1,20 @@
 data "aws_availability_zones" "azs" {}
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+output "eks_cluster_id" {
+  value = data.aws_eks_cluster.cluster.id
+}
+
+output "eks_cluster_name" {
+  value = data.aws_eks_cluster.cluster.name
+}
+
 
