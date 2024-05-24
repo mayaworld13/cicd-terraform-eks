416b49b2c1b268620beec30141b49e778edca06c
Mayank sharma <mayank14082003@gmail.com>
2024-05-24 12:28:41 +0530

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -56,11 +56,11 @@ module "eks" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster_auth" {
-  name = module.eks.cluster_name
+  name = module.eks.cluster_id
   depends_on = [module.eks]
 }
 
