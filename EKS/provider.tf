9dbbb086193f5dd106b5abfb6197d28275a668c7
Mayank sharma <mayank14082003@gmail.com>
2024-06-10 14:55:15 +0530

    Update provider.tf

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.20.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.1"
-    }
-  }
+provider "aws" {
+  region = "ap-south-1"
 }
